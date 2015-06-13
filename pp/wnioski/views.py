c4172f64422be88d33171d5b946b61f5f46b72c7
kekekekekeke <brodycarrie@gmail.com>
2015-06-13 19:03:07 +0200

    TG-logowanie #Done
    TG-logowanie #Done

--- pp/wnioski/views.py
+++ pp/wnioski/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.contrib.auth import authenticate, login, logout
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.db import models
